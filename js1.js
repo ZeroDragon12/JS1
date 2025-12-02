5f9249cc36028cb8c8a0361eafd799b605f0582e
ZeroDragon12 <klimi2004@gmail.com>
2025-12-03 02:26:01 +0500

    первое задание

---
+++ js1.js
@@ -0,0 +1,9 @@
+let Str = prompt('Введите день недели на русском').toLowerCase(); 
+if (Str=="понедельник") alert ('Monday'); 
+else if (Str=="вторник") alert ('Tuesday') 
+else if (Str=="среда") alert ('Wednesday') 
+else if (Str=="четверг") alert ('Thursday') 
+else if (Str=="пятница") alert ('Friday')
+else if (Str=="понедельник") alert ('Saturday') 
+else if (Str=="понедельник") alert ('Sunday') 
+else alert ('Ну ты даешь! Попробуй еще!'); 
